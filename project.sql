8e904316e9f93e2e619a89a9b1673ee18298cb7a
Dean Farrell <farrelldlfarrell@gmail.com>
2011-10-12 16:42:59 -0400

    Adding fuller SQL setup.  Uses rights module and tables didn't take into account case sensitivity on Windows vs. Linux.  May need to change table names to conform fully.

--- project.sql
+++ project.sql
@@ -1,5 +1,89 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Table structure for table `authassignment`
+--
+
+CREATE TABLE IF NOT EXISTS `AuthAssignment` (
+  `itemname` varchar(64) collate utf8_unicode_ci NOT NULL,
+  `userid` varchar(64) collate utf8_unicode_ci NOT NULL,
+  `bizrule` text collate utf8_unicode_ci,
+  `data` text collate utf8_unicode_ci,
+  PRIMARY KEY  (`itemname`,`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `authassignment`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authitem`
+--
+
+CREATE TABLE IF NOT EXISTS `authitem` (
+  `name` varchar(64) collate utf8_unicode_ci NOT NULL,
+  `type` int(11) NOT NULL,
+  `description` text collate utf8_unicode_ci,
+  `bizrule` text collate utf8_unicode_ci,
+  `data` text collate utf8_unicode_ci,
+  PRIMARY KEY  (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `authitem`
+--
+
+INSERT INTO `authitem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES
+('Authenticated', 2, NULL, NULL, 'N;'),
+('Guest', 2, NULL, NULL, 'N;');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authitemchild`
+--
+
+CREATE TABLE IF NOT EXISTS `authitemchild` (
+  `parent` varchar(64) collate utf8_unicode_ci NOT NULL,
+  `child` varchar(64) collate utf8_unicode_ci NOT NULL,
+  PRIMARY KEY  (`parent`,`child`),
+  KEY `child` (`child`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `authitemchild`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rights`
+--
+
+CREATE TABLE IF NOT EXISTS `rights` (
+  `itemname` varchar(64) collate utf8_unicode_ci NOT NULL,
+  `type` int(11) NOT NULL,
+  `weight` int(11) NOT NULL,
+  PRIMARY KEY  (`itemname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `rights`
+--
+
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `user`
 --
@@ -11,6 +95,11 @@ CREATE TABLE IF NOT EXISTS `user` (
   `password` varchar(64) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `user`
+--
+
 
 -- --------------------------------------------------------
 
@@ -24,3 +113,31 @@ CREATE TABLE IF NOT EXISTS `user_session_info` (
   `data` text collate utf8_unicode_ci,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `user_session_info`
+--
+
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `AuthAssignment`
+--
+ALTER TABLE `AuthAssignment`
+  ADD CONSTRAINT `authassignment_ibfk_1` FOREIGN KEY (`itemname`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `authitemchild`
+--
+ALTER TABLE `authitemchild`
+  ADD CONSTRAINT `authitemchild_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `authitemchild_ibfk_2` FOREIGN KEY (`child`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `rights`
+--
+ALTER TABLE `rights`
+  ADD CONSTRAINT `rights_ibfk_1` FOREIGN KEY (`itemname`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
