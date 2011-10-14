94264af743ef327ede33e862d840ddb55e72c27d
Dean Farrell <farrelldlfarrell@gmail.com>
2011-10-14 15:57:20 -0400

    Fixed SQL.  Rights module adds tables with Caps.  Causing havoc with Unix systems.

--- project.sql
+++ project.sql
@@ -1,12 +1,19 @@
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `authassignment`
 --
 
-CREATE TABLE IF NOT EXISTS `authassignment` (
+CREATE TABLE IF NOT EXISTS `AuthAssignment` (
   `itemname` varchar(64) collate utf8_unicode_ci NOT NULL,
   `userid` varchar(64) collate utf8_unicode_ci NOT NULL,
   `bizrule` text collate utf8_unicode_ci,
@@ -18,13 +25,14 @@ CREATE TABLE IF NOT EXISTS `authassignment` (
 -- Dumping data for table `authassignment`
 --
 
+
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `authitem`
 --
 
-CREATE TABLE IF NOT EXISTS `authitem` (
+CREATE TABLE IF NOT EXISTS `AuthItem` (
   `name` varchar(64) collate utf8_unicode_ci NOT NULL,
   `type` int(11) NOT NULL,
   `description` text collate utf8_unicode_ci,
@@ -37,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `authitem` (
 -- Dumping data for table `authitem`
 --
 
-INSERT INTO `authitem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES
+INSERT INTO `AuthItem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES
 ('Authenticated', 2, NULL, NULL, 'N;'),
 ('Guest', 2, NULL, NULL, 'N;');
 
@@ -62,10 +70,30 @@ CREATE TABLE IF NOT EXISTS `authitemchild` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `files_for_download`
+--
+
+CREATE TABLE IF NOT EXISTS `files_for_download` (
+  `id` int(10) NOT NULL auto_increment,
+  `url` varchar(500) collate utf8_unicode_ci NOT NULL,
+  `user_uploads_id` int(7) NOT NULL COMMENT "CONSTRAINT FOREIGN KEY (user_uploads_id) REFERENCES user_uploads(id)",
+  `user_id` int(6) NOT NULL COMMENT "CONSTRAINT FOREIGN KEY (user_id) REFERENCES user(id)",
+  `processed` tinyint(1) NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `files_for_download`
+--
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `rights`
 --
 
-CREATE TABLE IF NOT EXISTS `rights` (
+CREATE TABLE IF NOT EXISTS `Rights` (
   `itemname` varchar(64) collate utf8_unicode_ci NOT NULL,
   `type` int(11) NOT NULL,
   `weight` int(11) NOT NULL,
@@ -95,6 +123,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Dumping data for table `user`
 --
 
+
 -- --------------------------------------------------------
 
 --
@@ -120,15 +149,16 @@ CREATE TABLE IF NOT EXISTS `user_session_info` (
 
 CREATE TABLE IF NOT EXISTS `user_uploads` (
   `id` int(7) NOT NULL auto_increment,
-  `user_id` int(6) NOT NULL COMMENT 'CONSTRAINT FOREIGN KEY (user_id) REFERENCES user(id)',
+  `user_id` int(6) NOT NULL COMMENT "CONSTRAINT FOREIGN KEY (user_id) REFERENCES user(id)",
   `upload_path` varchar(250) collate utf8_unicode_ci NOT NULL,
   `processed` int(1) NOT NULL default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `user_uploads`
 --
+
 
 
 --
@@ -138,18 +168,18 @@ CREATE TABLE IF NOT EXISTS `user_uploads` (
 --
 -- Constraints for table `authassignment`
 --
-ALTER TABLE `authassignment`
-  ADD CONSTRAINT `authassignment_ibfk_1` FOREIGN KEY (`itemname`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AuthAssignment`
+  ADD CONSTRAINT `AuthAssignment_ibfk_1` FOREIGN KEY (`itemname`) REFERENCES `AuthItem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `authitemchild`
 --
 ALTER TABLE `authitemchild`
-  ADD CONSTRAINT `authitemchild_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `authitemchild_ibfk_2` FOREIGN KEY (`child`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `authitemchild_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `AuthItem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `authitemchild_ibfk_2` FOREIGN KEY (`child`) REFERENCES `AuthItem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `rights`
 --
-ALTER TABLE `rights`
-  ADD CONSTRAINT `rights_ibfk_1` FOREIGN KEY (`itemname`) REFERENCES `authitem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Rights`
+  ADD CONSTRAINT `Rights_ibfk_1` FOREIGN KEY (`itemname`) REFERENCES `AuthItem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
