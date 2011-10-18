bff4d1139650782bee496c4f68bee44dd5ea2df5
Dean Farrell <farrelldlfarrell@gmail.com>
2011-10-18 10:53:39 -0400

    Updated user controller to only allow access to logged in users

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -28,11 +28,11 @@ class UserController extends Controller
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
 				'actions'=>array('index','view'),
-				'users'=>array('*'),
+				'users'=>array('@'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
-				'users'=>array('@'),
+				'users'=>array('admin'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
