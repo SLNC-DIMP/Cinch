50d28fc4e43955e05cffa4fe9d9b9cb27215c65b
Michael Kimsal <mgkimsal@gmail.com>
2011-10-26 16:01:45 -0400

    use splfixedarray to save memory space - mgk

--- protected/commands/ReadFileCommand.php
+++ protected/commands/ReadFileCommand.php
@@ -18,12 +18,15 @@ class ReadFileCommand extends CConsoleCommand {
 	* Writes URL listings, user id and list id to files_for_download table
 	*/
 	public function addUrl($url, $user_uploads_id, $user_id) {
-		$sql = "INSERT INTO files_for_download(url, user_uploads_id, user_id) VALUES(:url, :user_uploads_id, :user_id)";
-		$write_files = Yii::app()->db->createCommand($sql);
-		$write_files->bindParam(":url", $url, PDO::PARAM_STR);
-		$write_files->bindParam(":user_uploads_id", $user_uploads_id, PDO::PARAM_INT);
-		$write_files->bindParam(":user_id", $user_id, PDO::PARAM_INT);
-		$write_files->execute();		
+		if(trim($url)!='')
+		{
+			$sql = "INSERT INTO files_for_download(url, user_uploads_id, user_id) VALUES(:url, :user_uploads_id, :user_id)";
+			$write_files = Yii::app()->db->createCommand($sql);
+			$write_files->bindParam(":url", $url, PDO::PARAM_STR);
+			$write_files->bindParam(":user_uploads_id", $user_uploads_id, PDO::PARAM_INT);
+			$write_files->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+			$write_files->execute();		
+		}
 	}
 	
 	/**
@@ -49,21 +52,20 @@ class ReadFileCommand extends CConsoleCommand {
 	*/
 	public function run() {
      $file_lists = $file_lists = $this->getLists();
-		
+     
 		foreach($file_lists as $file_list) {
-			$url_list = file($file_list['upload_path'], FILE_SKIP_EMPTY_LINES);
-			$url_count = $this->fileCount($url_list);
-			
-			$i = 0;
+			$url_list = SplFixedArray::fromArray(file($file_list['upload_path'], FILE_SKIP_EMPTY_LINES));
+
 			foreach($url_list as $url) {
-				$this->addUrl(strip_tags(trim($url)), $file_list['id'], $file_list['user_id']);
-				
-				$i++;
-			
-				if($i == ($url_count - 1)) {
-					$this->updateFileList($file_list['id']);
+				if(trim($url)=='')
+				{
+					continue;
 				}
+				
+				$this->addUrl(strip_tags(trim($url)), $file_list['id'], $file_list['user_id']);
+			
 			}
+			$this->updateFileList($file_list['id']);
 		} 	
     }
 }
