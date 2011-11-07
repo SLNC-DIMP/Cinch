8dcb768db2fdbccb145350e3ce4958b399199420
Dean Farrell <farrelldlfarrell@gmail.com>
2011-11-07 10:30:45 -0500

    file checksum creation should work correctly now.

--- protected/commands/ChecksumCommand.php
+++ protected/commands/ChecksumCommand.php
@@ -3,18 +3,33 @@ class ChecksumCommand extends CConsoleCommand {
 	public $table = 'file_info';
 	
 	/**
+	* Get list of downloaded files without checksums
+	*/
+	public function getFileList() {
+		$get_files = Yii::app()->db->createCommand()
+			->select("id, temp_file_path, user_id")
+			->from($this->table)
+			->where("checksum IS NULL")
+			->queryAll();
+			
+		return $get_files;
+	}
+	
+	/**
 	* Get count of files in file info table
 	* @return integer
 	*/
-	function getFileCount() {
+	public function getCheckedFileCount() {
 		$get_file_count = Yii::app()->db->createCommand()
-				->select("COUNT(*)")
-				->from($this->table)
-				->queryColumn();
+			->select("COUNT(*)")
+			->from($this->table)
+			->where("checksum IS NOT NULL")
+			->queryColumn();
 		
 		return $get_file_count[0];
 		
 	}
+	
 	/**
 	* Retrieves a list of checksums for files.  If more than 5000 files to check 
 	* it starts from a random point of less than or equal to 5000 files from the total # of files. 
@@ -44,10 +59,9 @@ class ChecksumCommand extends CConsoleCommand {
 	* @return object Data Access Object
 	*/
 	public function writeError($id) {
+		$sql = "UPDATE " . $this->table . " SET problem_file = 5 WHERE id = ?";
 		$write_error = Yii::app()->db->createCommand()
-			->update($this->table)
-			->set(array('problem_file' => 5))
-			->where('id = :id', array(':id' => $id));
+			->execute(array($id));
 	}
 	
 	/**
@@ -60,19 +74,48 @@ class ChecksumCommand extends CConsoleCommand {
 		return $checksum;
 	}
 	
+	public function writeSuccess($checksum, $id) {
+		$sql = "UPDATE " . $this->table . " SET checksum = ? WHERE id = ?";
+		$write = Yii::app()->db->createCommand($sql)
+			->execute(array($checksum, $id));
+	}
+	
+	
 	/**
-	* Run checksum command 
+	* Calculates a checksum for each file and compares it to the file's initial checksum
+	* Write error to DB if mismatch detected. 
 	*/
-	public function run() {
-		$file_count = $this->getFileCount();
+	public function actionMetaCheck() {
+		$file_count = $this->getCheckedFileCount();
 		
-		if($file_count != 0) {
+		if($file_count > 0) {
 			$file_list = $this->getFileChecksums($file_count);
 			
 			foreach($file_list as $file) {
-				$current_checksum = md5_file($file['temp_file_path']);
+				$current_checksum = $this->createChecksum($file['temp_file_path']);
 				if($current_checksum != $file['checksum']) {
 					$this->writeError($file['id']);
+				}
+			}
+		}
+	}
+	
+	/**
+	* Run checksum command 
+	* Default is to create new checksum for downloaded files
+	*/
+	public function run($args) {
+		$file_lists = $this->getFileList();
+		
+		if(count($file_lists) > 0) {
+			foreach($file_lists as $file_list) {
+				$checksum = $this->createChecksum($file_list['temp_file_path']);
+				if($checksum) {
+					$this->writeSuccess($checksum, $file_list['id']);
+					echo "checksum for:" . $file_list['temp_file_path'] . " is " . $checksum . "\r\n";
+				} else {
+					$this->writeError($file_list['id']);
+					echo "Checksum not created. for: " . $file_list['temp_file_path'] . "\r\n";
 				}
 			}
 		}
