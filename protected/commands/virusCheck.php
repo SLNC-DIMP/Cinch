f7566c10c9348ab4b99d055b46ccebe9de78bef7
Dean Farrell <farrelldlfarrell@gmail.com>
2011-11-15 13:52:22 -0500

    Adding basic virus check.  Actually virus check part doesn't work yet.

---
+++ protected/commands/virusCheck.php
@@ -0,0 +1,57 @@
+<?php
+class virusCheck extends CConsoleCommand {
+	/**
+	* Get file list for virus checks
+	* @access public
+	* @return object Yii DAO
+	*/
+	public function getFiles() {
+		$files = Yii::app()->db->createCommand()
+			->select('id, temp_file_path')
+			->from('file_info')
+			->where(':virus_check = virus_check', array(':virus_check' => 0))
+			->queryAll();
+			
+		return $files;
+	}
+	
+	/**
+	* Write file viruse checked and if error detected to the db
+	* @param $file_id
+	* @param $problem
+	* @access protected
+	* @return object Yii DAO
+	*/
+	protected function fileUpdate($file_id, $problem = 0) {
+		$sql = "UPDATE file_info SET virus_check = 1, problem_file = ? WHERE id = ?";
+		
+		$virus = Yii::app()->db->createCommand($sql);
+		$virus->execute(array($problem, $file_id));	
+	}
+	
+	/**
+	* Scan a file for viruses
+	* 11 is error code for Virus detected
+	* @param $file_path
+	* @param $file_id
+	* @access public
+	* @return mixed
+	*/
+	public function virusScan($file_path, $file_id) {
+		exec('' . $file_path, $output);
+		if($output == false) {
+			$this->fileUpdate($file_id, 11);
+			unlink($file_path);
+		} else {
+			$this->fileUpdate($file_id);
+		}
+	}
+	
+	public function run() {
+		$files = $this->getFiles();
+		
+		foreach($files as $file) {
+			$this->virusScan($file_path, $file_id);
+		}
+	}
+}
