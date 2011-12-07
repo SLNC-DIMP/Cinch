793c88a2767e2f06cdc033607c03da7e4556f186
Dean Farrell <farrelldlfarrell@gmail.com>
2011-12-07 16:35:23 -0500

    Adjust table names for metadata tables so active record will work correctly.

--- project.sql
+++ project.sql
@@ -84,10 +84,10 @@ INSERT INTO `AuthItem` (`name`, `type`, `description`, `bizrule`, `data`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `authitemchild`
+-- Table structure for table `AuthItemChild`
 --
 
-CREATE TABLE IF NOT EXISTS `authitemchild` (
+CREATE TABLE IF NOT EXISTS `AuthItemChild` (
   `parent` varchar(64) collate utf8_unicode_ci NOT NULL,
   `child` varchar(64) collate utf8_unicode_ci NOT NULL,
   PRIMARY KEY  (`parent`,`child`),
@@ -98,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `authitemchild` (
 -- Dumping data for table `authitemchild`
 --
 --
-INSERT INTO `authitemchild` (`parent`, `child`) VALUES
+INSERT INTO `AuthItemChild` (`parent`, `child`) VALUES
 ('Admin.Default.*', 'FileInfo.*'),
 ('Authenticated', 'Guest'),
 ('Admin.Default.*', 'Site.*'),
@@ -146,7 +146,7 @@ CREATE TABLE IF NOT EXISTS `csv_meta_paths` (
 -- Table structure for table `excel_metadata`
 --
 
-CREATE TABLE IF NOT EXISTS `excel_metadata` (
+CREATE TABLE IF NOT EXISTS `Excel_Metadata` (
   `id` int(7) NOT NULL auto_increment,
   `app_name` varchar(50) collate utf8_unicode_ci default NULL,
   `app_version` varchar(50) collate utf8_unicode_ci default NULL,
@@ -215,8 +215,8 @@ CREATE TABLE IF NOT EXISTS `file_info` (
   `last_modified` varchar(15) collate utf8_bin default NULL COMMENT 'file last modified timestamp',
   `zipped` int(1) NOT NULL default '0' COMMENT 'Yes/No added to a Zip Archive',
   `problem_file` int(1) NOT NULL default '0',
-  `user_id` int(6) NOT NULL default '0' COMMENT 'CONSTRAINT FOREIGN KEY (user_id) REFERENCES user(id)',
-  `upload_file_id` int(6) NOT NULL default '0' COMMENT 'CONSTRAINT FOREIGN KEY (upload_file_id) REFERENCES user_uploads(id)',
+  `user_id` int(6) NOT NULL default '0' COMMENT "CONSTRAINT FOREIGN KEY (user_id) REFERENCES user(id)",
+  `upload_file_id` int(6) NOT NULL default '0' COMMENT "CONSTRAINT FOREIGN KEY (upload_file_id) REFERENCES user_uploads(id)",
   `download_time` timestamp NOT NULL default CURRENT_TIMESTAMP COMMENT 'time file was downloaded for processing',
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='downloaded file information' AUTO_INCREMENT=1 ;
@@ -249,8 +249,9 @@ CREATE TABLE IF NOT EXISTS `files_for_download` (
 -- Table structure for table `jpg_metadata`
 --
 
-CREATE TABLE IF NOT EXISTS `jpg_metadata` (
+CREATE TABLE IF NOT EXISTS `Jpg_Metadata` (
   `id` int(12) NOT NULL auto_increment,
+  `author` varchar(255) collate utf8_unicode_ci NOT NULL,
   `color_space` varchar(25) collate utf8_unicode_ci default NULL,
   `component_one` varchar(150) collate utf8_unicode_ci default NULL,
   `component_two` varchar(150) collate utf8_unicode_ci default NULL,
@@ -273,10 +274,6 @@ CREATE TABLE IF NOT EXISTS `jpg_metadata` (
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `jpg_metadata`
---
-
 
 -- --------------------------------------------------------
 
@@ -284,7 +281,7 @@ CREATE TABLE IF NOT EXISTS `jpg_metadata` (
 -- Table structure for table `pdf_metadata`
 --
 
-CREATE TABLE IF NOT EXISTS `pdf_metadata` (
+CREATE TABLE IF NOT EXISTS `Pdf_Metadata` (
   `id` int(10) NOT NULL auto_increment,
   `author` varchar(250) collate utf8_bin default NULL,
   `creation_date` varchar(30) collate utf8_bin default NULL COMMENT 'file creation date',
@@ -315,13 +312,13 @@ CREATE TABLE IF NOT EXISTS `pdf_metadata` (
 CREATE TABLE IF NOT EXISTS `problem_files` (
   `id` int(7) NOT NULL auto_increment,
   `error_id` int(3) NOT NULL,
-  `file_id` int(10) default NULL,
+  `file_id` int(10) NOT NULL,
   `user_id` int(7) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `error_id` (`error_id`),
   KEY `file_id` (`file_id`),
   KEY `user_id` (`user_id`)
-)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Table structure for table `rights`
@@ -398,7 +395,7 @@ CREATE TABLE IF NOT EXISTS `upload` (
 -- Table structure for table `word_metadata`
 --
 
-CREATE TABLE IF NOT EXISTS `word_metadata` (
+CREATE TABLE IF NOT EXISTS `Word_Metadata` (
   `id` int(12) NOT NULL auto_increment,
   `app_name` varchar(50) collate utf8_unicode_ci default NULL,
   `app_version` varchar(50) collate utf8_unicode_ci default NULL,
@@ -461,9 +458,9 @@ ALTER TABLE `AuthAssignment`
 --
 -- Constraints for table `authitemchild`
 --
-ALTER TABLE `authitemchild`
-  ADD CONSTRAINT `authitemchild_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `AuthItem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `authitemchild_ibfk_2` FOREIGN KEY (`child`) REFERENCES `AuthItem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AuthItemChild`
+  ADD CONSTRAINT `AuthItemChildibfk_1` FOREIGN KEY (`parent`) REFERENCES `AuthItem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `AuthItemChild_ibfk_2` FOREIGN KEY (`child`) REFERENCES `AuthItem` (`name`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `rights`
@@ -475,7 +472,7 @@ ALTER TABLE `Rights`
 -- Constraints for table `problem_files`
 --
 ALTER TABLE `problem_files`
-  ADD CONSTRAINT `problem_files_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `problem_files_ibfk_1` FOREIGN KEY (`error_id`) REFERENCES `error_type` (`id`),
-  ADD CONSTRAINT `problem_files_ibfk_2` FOREIGN KEY (`file_id`) REFERENCES `file_info` (`id`);
+  ADD CONSTRAINT `problem_files_ibfk_2` FOREIGN KEY (`file_id`) REFERENCES `file_info` (`id`),
+  ADD CONSTRAINT `problem_files_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
  
