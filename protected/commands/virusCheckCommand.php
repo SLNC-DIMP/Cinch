327d2021e57cc414cc3da4a7cdeecda9c2e5d03e
Dean Farrell <farrelldlfarrell@gmail.com>
2012-01-20 13:43:09 -0500

    Corrected virus check to select only downloaded files not all files in the download list.

--- protected/commands/virusCheckCommand.php
+++ protected/commands/virusCheckCommand.php
@@ -9,8 +9,8 @@ class virusCheckCommand extends CConsoleCommand {
 		$files = Yii::app()->db->createCommand()
 			->select('id, temp_file_path')
 			->from('file_info')
-			->where(':virus_check = virus_check or :problem_file = problem_file', 
-			  array(':virus_check' => 0, ':problem_file' => 1))
+			->where(':virus_check = virus_check AND :temp_file_path != temp_file_path', 
+			  array(':virus_check' => 0, ':temp_file_path' => ''))
 			->queryAll();
 			
 		return $files;
