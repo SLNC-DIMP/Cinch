02c43bfc87c812d16616b0b8aa26dcf7c8e018d5
Dean Farrell <farrelldlfarrell@gmail.com>
2012-02-28 17:01:33 -0500

    switched to using clamAV daemon for virus checking. Way faster. Added writing of virus scan event.

--- protected/commands/virusCheckCommand.php
+++ protected/commands/virusCheckCommand.php
@@ -1,5 +1,5 @@
 <?php
-Yii::import('application.models.ErrorFiles');
+Yii::import('application.models.Utils');
 
 class virusCheckCommand extends CConsoleCommand {
 	/**
@@ -28,6 +28,10 @@ class virusCheckCommand extends CConsoleCommand {
 	protected function fileUpdate($file_id, $problem = 0) {
 		$sql = "UPDATE file_info SET virus_check = 1, problem_file = ? WHERE id = ?";
 		
+		if($problem > 0) {
+			$sql = "UPDATE file_info SET temp_file_path = '', virus_check = 1, problem_file = ? WHERE id = ?";
+		}
+		
 		$virus = Yii::app()->db->createCommand($sql);
 		$virus->execute(array($problem, $file_id));	
 	}
@@ -38,21 +42,21 @@ class virusCheckCommand extends CConsoleCommand {
 	* @access private
 	*/
 	private function updateDefs() {
-		system(escapeshellcmd('freshclam'));
+		system(escapeshellcmd('sudo freshclam'));
 	}
 	
 	/**
 	* Scan a file for viruses
-	* 11 is error code for Virus detected
-	* 14 error for unable to delete file
+	* 4 event code for virus scan run
 	* @param $file_path
 	* @param $file_id
 	* @access public
 	* @return mixed
 	*/
-	public function virusScan($file_path) {
-		exec('clamscan ' . $file_path, $output);
-	 
+	public function virusScan($file_path, $file_id) {
+		exec('clamdscan ' . $file_path, $output);
+	 	Utils::writeEvent($file_id, 4);
+		
 		return $output;	
 	}
 	
@@ -65,7 +69,8 @@ class virusCheckCommand extends CConsoleCommand {
 	*/
 	private function scanOutput(array $output) {
 		$file_path = preg_replace('/:\s{1,}ok$/i', '', $output[0]);
-		$num_infected = substr_replace(strrchr($output[7], ':'), '', 0, 2);
+		//$num_infected = substr_replace(strrchr($output[7], ':'), '', 0, 2);
+		$num_infected = substr_replace(strrchr($output[3], ':'), '', 0, 2);
 		
 		return array('path' => $file_path, 'infected' => $num_infected);
 	}
@@ -74,6 +79,7 @@ class virusCheckCommand extends CConsoleCommand {
 	* Write scan results
 	* 11 is error code for Virus detected
 	* 14 error for unable to delete file
+	* 16 Virus check couldn't scan file
 	* @param $scan_results
 	* @param $file_id
 	* @param $user_id
@@ -81,20 +87,29 @@ class virusCheckCommand extends CConsoleCommand {
 	*/
 	private function writeScan(array $scan_results, $file_id, $user_id) {
 		$scan = $this->scanOutput($scan_results);
-		if($scan['infected'] > 0) {
-			ErrorFiles::writeError(11, $file_list['id'], $file_list['user_id']);
-			$this->fileUpdate($file_id, 1);
+		
+		if(empty($scan) || $scan['infected'] > 0) {
+			if(empty($scan)) {
+				$error_id = 16;
+				$message_text = "Scan failed";
+			} else {
+				$error_id = 11;
+				$message_text = "Virus detected";
+			}
+			Utils::writeError($error_id, $file_list['id'], $file_list['user_id']);
 			
 			$delete = @unlink($scan['path']);
 			if(!$delete) {
-				ErrorFiles::writeError(14, $file_list['id'], $file_list['user_id']);
-				echo "Virus detected, but not deleted! \r\n";
+				Utils::writeError(14, $file_list['id'], $file_list['user_id']);
+				$this->fileUpdate($file_id, 1);
+				echo $message_text . ", but file could not be deleted! - $file_id  \r\n";
 			} else {
-				echo "Virus detected! \r\n";
+				$this->fileUpdate($file_id, 1);
+				echo $message_text . "! File deleted - $file_id \r\n";
 			}
 		} else {
 			$this->fileUpdate($file_id);
-			echo "No virus detected \r\n";
+			echo "No virus detected - $file_id \r\n";
 		}
 	}
 	
@@ -102,14 +117,10 @@ class virusCheckCommand extends CConsoleCommand {
 		$files = $this->getFiles();
 		if(empty($files)) { exit; }
 		
-		$this->updateDefs();
+	//	$this->updateDefs();
 		foreach($files as $file) {
-			$scan = $this->virusScan($file['temp_file_path']);
-			if(!empty($scan)) {
-				$this->writeScan($scan, $file['id'], $file['user_id']);
-			} else {
-				echo "scan failed\r\n";
-			}
+			$scan = $this->virusScan($file['temp_file_path'], $file['id']);
+			$this->writeScan($scan, $file['id'], $file['user_id']);
 		}
 	}
 }
