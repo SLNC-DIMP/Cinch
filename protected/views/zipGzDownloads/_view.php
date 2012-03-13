c7843539288de38e99653ffad5d17b2674dddb46
Dean Farrell <farrelldlfarrell@gmail.com>
2012-03-13 11:25:40 -0400

    Cleaned up download view to have it make more sense to the user.  Added date_time of download to each entry's view.

--- protected/views/zipGzDownloads/_view.php
+++ protected/views/zipGzDownloads/_view.php
@@ -1,7 +1,7 @@
 <div class="view">
 
-	<b>Download File:<br /></b>
-	<?php echo CHtml::link('Download this File', CHtml::encode('zipGzDownloads/download/'. $data->id)); ?>
+	<b>Download zip file (created on <?php echo CHtml::encode($data->creationdate); ?>):<br /></b>
+	<?php echo CHtml::link('Download this file' , CHtml::encode('zipGzDownloads/download/'. $data->id)); ?>
 	<br />
 
 </div>
--- protected/views/zipGzDownloads/index.php
+++ protected/views/zipGzDownloads/index.php
@@ -9,7 +9,7 @@ $this->menu=array(
 ?>
 
 <h1>Your Available Downloads</h1>
-
+<h5>Note: the contents of each file should be unique</h5>
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
