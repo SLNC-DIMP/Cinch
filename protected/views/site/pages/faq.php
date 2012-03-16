ec292391a7a8c9e9ccb3eb23d96f16805c25530c
Dean Farrell <farrelldlfarrell@gmail.com>
2012-03-16 15:02:48 -0400

    Adding NC hosted specific files

---
+++ protected/views/site/pages/faq.php
@@ -0,0 +1,16 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - FAQ';
+$this->breadcrumbs=array(
+	'FAQ',
+);
+?>
+<h1>FAQ</h1>
+<p>Note: This page is very much a work in progress.</p>
+
+<h3>How can I use Cinch?</h3>
+<p>We're currently in alpha testing with a small group of users.  When Cinch is released live if your institution is a library or other cultural entity in the state of North Carolina you can take advantage of our hosted service.
+Please send an email to the Cinch administrative team (<a href="mailto:cinch_admin@cinch.nclive.org">Cinch Admin</a>) and we'll set you up with an account.  If your are not a North Carolina based cultural institution or would prefer to host a Cinch
+instance yourself please see our <a href="https://github.com/lfarrell/Cinch">GitHub page</a> for instructions on installing Cinch.</p>
+
+<h3>Why can my upload list only have 4,500 files listed for Cinch to collect?</h3>
+<p>This is because our server has problems dealing with a large number files at one pass.  However feel free to upload as many lists as you like.  We'll get to them all.</p>
