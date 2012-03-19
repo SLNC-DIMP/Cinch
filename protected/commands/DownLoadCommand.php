3c26aa8a485b062e5e0f28c22fd50991c0f1dfa6
Dean Farrell <farrelldlfarrell@gmail.com>
2012-03-19 13:41:18 -0400

    Lowered timeout limits so failed downloads return sooner

--- protected/commands/DownLoadCommand.php
+++ protected/commands/DownLoadCommand.php
@@ -332,8 +332,8 @@ class DownloadCommand extends CConsoleCommand {
 			curl_setopt($ch, CURLOPT_HEADER, 0);
 			curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
-			curl_setopt($ch, CURLOPT_TIMEOUT, 360);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 7);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 			curl_setopt($ch, CURLOPT_FILETIME, 1);
 						
 			curl_exec($ch);
