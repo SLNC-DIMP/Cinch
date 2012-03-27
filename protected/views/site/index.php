ea699fb100a430a22af6ebe1228c3913dbdfe3d6
Dean Farrell <farrelldlfarrell@gmail.com>
2012-03-27 16:33:46 -0400

    spruced up the site look.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,9 +1,18 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<!-- <h1>Welcome to <i><?php // echo CHtml::encode(Yii::app()->name); ?></i></h1> -->
+<div id="cinch_image"><?php echo CHtml::image(Yii::app()->request->baseUrl.'/images/cinch.png'); ?></div>
 
-<p>Congratulations on choosing CINCH!</p>
+<div id="cinch_text">
+<p>
+The CINCH project will develop a tool that allows users to bulk download files that are publicly available on the internet to their local storage. The benefit of the CINCH tool is its ability to retrieve large numbers of files in a preservation friendly way.  How does CINCH work? Users:</p>
+<ol>
+	<li>Upload a list of URLs to files they want the CINCH tool to retrieve.</li>
+	<li>Run the tool.</li>
+    <li>Download the retrieved files and file metadata to their local environment.</li>
+</ol>
 
-
-
-<p>A project to develop a bulk download service to a central repository that will maintain original file timestamps, extract file level metadata, create file checksums and periodically validate checksums for continued file integrity. Users merely need to upload a list of URLs to download or an FTP address and when the process completes they can download the requested files and file metadata to their local environment. Funding for the CINCH: Capture, Ingest, &amp; Checksum tool is made possible through an IMLS Sparks! Ignition grant.</p>
+<p>The tool will maintain original file timestamps; rename file names with a unique, intelligent identifier; run virus scans; extract file-level metadata; create file checksums; validate checksums for file integrity; and track all actions in an audit trail document. With repeated uses, the CINCH tool will track file duplicates.<p>
+ 
+<p>Funding for the CINCH: Capture, Ingest, &amp; Checksum tool is made possible through an IMLS Sparks! Ignition grant.</p>
+</div>
