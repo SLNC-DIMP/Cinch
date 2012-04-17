bb953c8e6627e7a185a68302a0e5d9fc4f85a17d
Dean Farrell <farrelldlfarrell@gmail.com>
2012-04-17 15:06:53 -0400

    Added more fixtures.

---
+++ protected/tests/fixtures/text_metadata.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+  'text_metadata1' => array('content_encoding'=>'ISO-8859-1',
+'file_size'=>'64',
+'file_name'=>'statelibrarync.org_cinch_test_header_with_delay.txt',
+'possible_doc_title'=>NULL,
+'possible_doc_keywords'=>NULL,
+'file_id'=>'19',
+'user_id'=>'1'),
+);
---
+++ protected/tests/fixtures/zip_gz_downloads.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+  'zip_gz_downloads' => array('user_id'=>'1',
+'path'=>'/srv/www/html/Cinch/protected/curl_downloads/lfarrell/lfarrell_downloads_2012_04_17.zip',
+'downloaded'=>'0',
+'deletion_reminder'=>'0',
+'creationdate'=>'2012-04-17 13:59:06')
+);
