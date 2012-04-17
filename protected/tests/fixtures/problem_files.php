7f5f7457317db4d13205d143c63f7463d6177d52
Dean Farrell <farrelldlfarrell@gmail.com>
2012-04-17 17:06:10 -0400

    Added problem files fixture

---
+++ protected/tests/fixtures/problem_files.php
@@ -0,0 +1,147 @@
+<?php
+return array(
+  'problem_file1' => array(
+'error_id'=>'1',
+'file_id'=>'18',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 11:30:16'),
+
+  'problem_file2' => array(
+'error_id'=>'1',
+'file_id'=>'291',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 11:32:34'),
+
+  'problem_file3' => array(
+'error_id'=>'1',
+'file_id'=>'1556',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 11:43:41'),
+
+  'problem_file4' => array(
+'error_id'=>'1',
+'file_id'=>'1661',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:16:44'),
+
+  'problem_file5' => array(
+'error_id'=>'1',
+'file_id'=>'1676',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:19:32'),
+
+  'problem_file6' => array(
+'error_id'=>'1',
+'file_id'=>'1677',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:19:33'),
+
+  'problem_file7' => array(
+'error_id'=>'3',
+'file_id'=>'1540',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:50'),
+
+  'problem_file8' => array(
+'error_id'=>'17',
+'file_id'=>'1540',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:50'),
+
+  'problem_file9' => array(
+'error_id'=>'3',
+'file_id'=>'1541',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:50'),
+
+  'problem_file10' => array(
+'error_id'=>'17',
+'file_id'=>'1541',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:50'),
+
+  'problem_file11' => array(
+'error_id'=>'3',
+'file_id'=>'1542',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:50'),
+
+  'problem_file12' => array(
+'error_id'=>'17',
+'file_id'=>'1542',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:50'),
+
+  'problem_file13' => array(
+'error_id'=>'3',
+'file_id'=>'1543',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file14' => array(
+'error_id'=>'17',
+'file_id'=>'1543',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file15' => array(
+'error_id'=>'3',
+'file_id'=>'1544',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file16' => array(
+'error_id'=>'17',
+'file_id'=>'1544',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file17' => array(
+'error_id'=>'3',
+'file_id'=>'1545',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file18' => array(
+'error_id'=>'17',
+'file_id'=>'1545',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file19' => array(
+'error_id'=>'3',
+'file_id'=>'1546',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file20' => array(
+'error_id'=>'17',
+'file_id'=>'1546',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file21' => array(
+'error_id'=>'3',
+'file_id'=>'1547',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file22' => array(
+'error_id'=>'17',
+'file_id'=>'1547',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file23' => array(
+'error_id'=>'3',
+'file_id'=>'1548',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51'),
+
+  'problem_file24' => array(
+'error_id'=>'17',
+'file_id'=>'1548',
+'csv_added'=>'1',
+'problem_time'=>'2012-04-17 12:48:51')
+
+);
