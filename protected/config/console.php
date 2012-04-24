8d72df76005465f907b5bffe21c9512576c570a8
Dean Farrell <farrelldlfarrell@gmail.com>
2012-04-24 13:37:13 -0400

    updating console.php

--- protected/config/console.php
+++ protected/config/console.php
@@ -1,67 +1,38 @@
 <?php
-// uncomment the following to define a path alias
-// Yii::setPathOfAlias('local','path/to/local-folder');
+// This is the configuration for yiic console application.
+// Any writable CConsoleApplication properties can be configured here.
+date_default_timezone_set('America/New_York');
 
-// This is the main Web application configuration. Any writable
-// CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Cinch - Digital Preservation Made...Easier!',
-
-	// preloading 'log' component
+	'name'=>'Cron',
+	
 	'preload'=>array('log'),
-
-	// autoloading model and component classes
-	'import'=>array(
-		'application.models.*',
-		'application.components.*',
-		// used by rights module
-		'application.modules.rights.*', 
-		'application.modules.rights.components.*',
-	),
-
-	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-	 /*	'gii'=>array(
-			'class'=>'system.gii.GiiModule',
-			'password'=>'',
-		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
-		), */
-		'admin',
-		// used by rights module
-		'rights'=>array(
-			//'install'=>true,
-			//'superuserName'=>''
-		),
-	),
-
+ 
+    'import'=>array(
+        'application.components.*',
+        'application.models.*',
+    ),
+	
 	// application components
 	'components'=>array(
-		'user'=>array(
-			'class' => 'RWebUser',
-			// enable cookie-based authentication
-			'allowAutoLogin'=>false,
-		),
-		'authManager'=>array(
-			'class'=>'RDbAuthManager',   // provides support for authorization item sorting
-			'defaultRoles'=>array('authenticated', 'guest'),                        
-		),
-		// adding in CSRF protection component
-		'request' => array(
-			'enableCsrfValidation' => true
-		),
-		// uncomment the following to enable URLs in path-format
-		'urlManager'=>array(
-			'urlFormat'=>'path',
-			'rules'=>array(
-				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
-				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
-				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
-			),
-		),
-		/*
-		'db'=>array(
+		'log'=>array(
+            'class'=>'CLogRouter',
+            'routes'=>array(
+                array(
+                    'class'=>'CFileLogRoute',
+                    'logFile'=>'cron.log',
+                    'levels'=>'error, warning',
+                ),
+                array(
+                    'class'=>'CFileLogRoute',
+                    'logFile'=>'cron_trace.log',
+                    'levels'=>'trace',
+                ),
+            ),
+        ),
+		
+		/*'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		), */
 		// uncomment the following to use a MySQL database
@@ -72,39 +43,5 @@ return array(
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		
-		// added to put session info into db
-		'session' => array (
-			'class' => 'system.web.CDbHttpSession',
-			'connectionID' => 'db',
-			'sessionTableName' => 'user_session_info',
-		),
-	
-		'errorHandler'=>array(
-			// use 'site/error' action to display errors
-            'errorAction'=>'site/error',
-        ),
-		'log'=>array(
-			'class'=>'CLogRouter',
-			'routes'=>array(
-				array(
-					'class'=>'CFileLogRoute',
-					'levels'=>'error, warning',
-				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				), */
-				
-			),
-		),
-	),
-
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
-	'params'=>array(
-		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
 	),
 );
