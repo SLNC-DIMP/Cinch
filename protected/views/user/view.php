5a8e91f6b8b8522ef0e48e9e79f5c576de182ff1
Dean Farrell <farrelldlfarrell@gmail.com>
2012-05-15 14:42:34 -0400

    Fixed bug where users couldn't be deleted due to csrf token mismatch.

--- protected/views/user/view.php
+++ protected/views/user/view.php
@@ -8,7 +8,7 @@ $this->menu=array(
 	array('label'=>'List User', 'url'=>array('index')),
 	array('label'=>'Create User', 'url'=>array('create')),
 	array('label'=>'Update User', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete User', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Delete User', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?','csrf'=>true)),
 	array('label'=>'Manage User', 'url'=>array('admin')),
 );
 ?>
