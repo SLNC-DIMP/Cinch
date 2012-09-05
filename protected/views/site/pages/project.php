d2a445409148ba0490c1b562947ca99371608d37
Dean Farrell <farrelldlfarrell@gmail.com>
2012-09-05 11:05:57 -0400

    Adding unlicense file and project page file.

---
+++ protected/views/site/pages/project.php
@@ -0,0 +1,36 @@
+<?php
+$this->pageTitle=Yii::app()->name . ' - About the Project';
+$this->breadcrumbs=array(
+	'About the Project',
+);
+?>
+<h2>CINCH Project</h2>
+<p>Developed by the State Library of North Carolina (part of the North Carolina Department of Cultural Resources), CINCH was funded by a 2011 <a href="http://www.imls.gov/applicants/grants/SparksIgnition.shtm">Institute of Museum and Library Services Sparks! Ignition grant</a>. </p>
+<h3>Institutional Partners</h3>
+<ul>
+  <li><a href="http://www.elon.edu/e-web/library/">Elon University, Belk Library</a></li>
+  <li><a href="http://nclive.org/">NC LIVE (North Carolina Libraries for Virtual Education)</a></li>
+  <li><a href="http://archives.ncdcr.gov/">North Carolina State Archives</a></li>
+  <li><a href="http://statelibrary.ncdcr.gov/">State Library of North Carolina</a> (lead)</li>
+  <li><a href="http://library.uncc.edu/">University of North Carolina at Charlotte, J. Murrey Atkins Library</a></li>
+</ul>
+<h3>Project Staff</h3>
+<ul>
+  <li> Kelly Eubank. North Carolina State Archives</li>
+  <li> Dean Farrell. State Library of North Carolina</li>
+  <li> Katie McCormick. University of North Carolina at Charlotte, J. Murrey Atkins Library</li>
+  <li> Katie Nash. Elon University, Belk Library</li>
+  <li>Scott Ross. North Carolina Libraries for Virtual Education</li>
+  <li> <a href="http://statelibrary.ncdcr.gov/about/staff.html#rudersdorf" target="_blank">Amy Rudersdorf</a>. State Library of North Carolina (PI)</li>
+</ul>
+<h3>Publications &amp; Presentations</h3>
+<h5>2012</h5>
+<p>Digital preservation ingest can be a &quot;CINCH.&quot; <em>Library Hi-Tech</em>. Forthcoming, July 2012</p>
+<p><a href="http://blogs.loc.gov/digitalpreservation/2012/06/digital-preservation-is-a-cinch/">Digital preservation is a CINCH</a>. June 2012. The Signal blog.</p>
+<p><a href="http://digitalpreservation.ncdcr.gov/asgii/presentations/ala2012.pdf">Collecting  born-digital materials from the web: It's a CINCH!</a> (.pdf) Digital Preservation Interest Group, American Library Association Annual Meeting. June 2012. Anaheim, CA.</p>
+<p><a href="http://digitalpreservation.ncdcr.gov/asgii/presentations/jcdl2012.pdf">Electronic records processing: It's a CINCH!</a> (.pdf) Joint Conference on Digital Libraries. June 2012. Washington DC.</p>
+<p><a href="http://prezi.com/gvg2ojbfoect/harvesting-the-harvest-iipc-april-30-2012-washington-dc/" target="_blank">Harvesting from the harvest: Automatic extraction of state government publications from web archives</a>. International Internet Preservation Consortium. April 30, 2012. Washington DC.</p>
+<h5>2011</h5>
+<p><a href="http://www.imls.gov/news/2011/050311.shtm" target="_blank">Press release</a>. Institute for Museum and Library Services.</p>
+<p><a href="http://news.ncdcr.gov/2011/05/18/state-library-receives-national-sparks-ignition-grant/" target="_blank">Press release</a>. North Carolina Department of Cultural Resources. </p>
+<h1>&nbsp;</h1>
