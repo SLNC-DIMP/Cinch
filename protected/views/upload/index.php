ac13fe7e6b6d417ceac3e1cb074f2282a9ce140a
Dean Farrell <farrelldlfarrell@gmail.com>
2012-09-05 11:41:29 -0400

    fixing branch merging.

--- protected/views/upload/index.php
+++ protected/views/upload/index.php
@@ -15,7 +15,9 @@ $this->breadcrumbs=array(
     <?php echo CHtml::error($model, 'urls_in_list'); ?>
     <br />
     <?php echo CHtml::activeFileField($model, 'path'); ?>
+  
     <br /><br />
     <?php echo CHtml::submitButton('Upload'); ?>
     <?php echo CHtml::endForm(); ?>
+   
 </div>
