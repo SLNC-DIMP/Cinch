7330310a5f2bc0b5e6aadbbee7843cb3e4bfe2f8
Dean Farrell <farrelldlfarrell@gmail.com>
2012-10-01 17:03:31 -0400

    rearranged ordering of upload form. Added padding to force conversion form element.

--- js/upload_form.js
+++ js/upload_form.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
         if($('#Upload_pdfa:checked').val() == 1) {
             hide.removeClass('hide');
+            $('#pdfa_select').css('margin-bottom', '20px');
         } else {
            hide.addClass('hide');
            $('#Upload_pdfa_convert_1').attr('checked', 'checked');
--- protected/views/upload/index.php
+++ protected/views/upload/index.php
@@ -16,26 +16,27 @@ $this->breadcrumbs=array(
     <?php echo CHtml::error($model, 'path'); ?>
     <?php echo CHtml::error($model, 'urls_in_list'); ?>
     <br />
-    <?php echo CHtml::activeFileField($model, 'path'); ?>
-  	<br /><br />
-    <h5>Select whether images and/or pdfs should be converted to preservation formats (optional): </h5>
-    <?php echo CHtml::activeLabel($model, 'JPEG2000'); ?>
-    <?php echo CHtml::activeCheckbox($model, 'jp2'); ?>
-    
-    <?php echo CHtml::activeLabel($model, 'PDFA'); ?>
-    <?php echo CHtml::activeCheckbox($model, 'pdfa'); ?>
-    <br /><br />
-    <div id="pdfa_select" class='hide'>
-    <h5>Force PDF/a conversion: </h5>
-    <?php echo CHtml::activeRadiobuttonList($model, 'pdfa_convert', array(1=>'Yes', 0=>'No'), array('separator'=> '')); ?>
-    </div>
-    <br /><br />
+     <?php echo CHtml::activeFileField($model, 'path'); ?>
+     <br /><br />
     <h5>Select file checksum type: </h5>
+
     <?php echo CHtml::activeRadiobuttonList($model, 'checksum_type', array(1=>'SHA1', 2=>'MD5'), array('separator'=> '')); ?>
     <br /><br />
     <h5>Select file download type: </h5>
     <?php echo CHtml::activeRadiobuttonList($model, 'download_type', array(1=>'Zip', 2=>'Bagit (tar.gz compression)'), array('separator'=> '')); ?>
     <br /><br />
+    <h5>Select whether images and/or pdfs should be converted to preservation formats (optional): </h5>
+    <?php echo CHtml::activeLabel($model, 'JPEG2000'); ?>
+    <?php echo CHtml::activeCheckbox($model, 'jp2'); ?>
+
+    <?php echo CHtml::activeLabel($model, 'PDFA'); ?>
+    <?php echo CHtml::activeCheckbox($model, 'pdfa'); ?>
+     <br /><br />
+     <div id="pdfa_select" class='hide'>
+        <h5>Force PDF/a conversion: </h5>
+        <?php echo CHtml::activeRadiobuttonList($model, 'pdfa_convert', array(1=>'Yes', 0=>'No'), array('separator'=> '')); ?>
+     </div>
+
     <?php echo CHtml::submitButton('Upload'); ?>
     <?php echo CHtml::endForm(); ?>
    </div>
