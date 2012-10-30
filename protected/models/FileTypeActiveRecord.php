3baba437d83d0594112f0ee7f493197b4cae39bd
Dean Farrell <farrelldlfarrell@gmail.com>
2012-10-30 15:54:16 -0400

    Fixed PHPDoc comments

--- protected/models/FileTypeActiveRecord.php
+++ protected/models/FileTypeActiveRecord.php
@@ -9,9 +9,9 @@ abstract class FileTypeActiveRecord extends CActiveRecord {
 	
 	/**
 	* Returned metadata fields vary by document, not just doc type.
-	* This finds the interection of returned metadata with file type table fields.
-	* @param $possible_query_fields
-	* @param $metadata
+	* This finds the intersection of returned metadata with file type table fields.
+	* @param array $possible_query_fields
+	* @param array $metadata
 	* @access public
 	* @return array
 	*/
@@ -22,7 +22,7 @@ abstract class FileTypeActiveRecord extends CActiveRecord {
 	/**
 	* Flattens table fields into a string for query building.
 	* Adds : if creating prepared statement bindings.
-	* @param $fields
+	* @param  array $fields
 	* @param $prepare
 	* @access public
 	* @return string
@@ -39,8 +39,8 @@ abstract class FileTypeActiveRecord extends CActiveRecord {
 	/**
 	* Generates bind parameters on queries and cleans field values for insertion.
 	* Each tika value starts with : so need to remove it.
-	* @param $fields
-	* @param $metadata
+	* @param array $fields
+	* @param array $metadata
 	* @access public
 	* @return array
 	*/
@@ -61,7 +61,7 @@ abstract class FileTypeActiveRecord extends CActiveRecord {
 	
 	/**
 	* Merges file and/or user id info onto the end of the metadata array
-	* @param $metadata_fields
+	* @param array $metadata_fields
 	* @param $id_values
 	* @access public
 	* @return array
